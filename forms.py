031ca464ebd76e3b5e5b75db39cb14b4338ebca3
Miguel Bohórquez <dlsg1009@gmail.com>
2023-09-08 23:50:30 -0500

    Agregando archivo forms.py

--- forms.py
+++ forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField("Nombre:", validators=[DataRequired(message="Ingresa tu nombre")])
-    password = PasswordField("Contraseña:", validators=[DataRequired(message="Ingresa tu password"), Length(min=4, max=16)])
+    password = PasswordField("Contraseña:", validators=[DataRequired(message="Ingresa tu password"), Length(min=4, max=16, message="La contraseña debe tener entre 4 y 16 caracteres")])
     submit = SubmitField("Enviar")
