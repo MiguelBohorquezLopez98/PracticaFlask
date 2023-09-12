33aec96b01cd0d9f98159b26b0ec5c31f05ab5b2
Miguel Bohórquez <dlsg1009@gmail.com>
2023-09-11 21:35:38 -0500

    Agregando archivo main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,13 @@
-console.log("Fundamentos Flask")
-alert("Hola")
+const $signinBtn = document.getElementById("signin");
+const $login = document.getElementById("login-container");
+
+$signinBtn.addEventListener("click", () => {
+    $login.style.display = "block";
+});
+
+document.onkeydown = function(e) {
+    e = e || window.event;
+    if (e.key === "Escape") {
+        $login.style.display = "none";
+    }
+};
