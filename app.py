d77d8cd9c37b3f79a7e58e829b41ef6e1392dd00
Miguel Bohórquez <dlsg1009@gmail.com>
2023-09-11 21:48:45 -0500

    Agregando app.py

--- app.py
+++ app.py
@@ -1,49 +1,31 @@
-from flask import Flask, redirect, url_for, request, render_template, \
-    flash, session
-from forms import LoginForm
+from flask import Flask, render_template, request, redirect, url_for
+
+from forms import LoginForm, SignupForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "so-secret"
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
-    
+    return redirect(url_for('signin'))
 
-@app.route("/contacto/")
-def contact():
-    return """
-    <h1 style="font-size: 64px; color:blue;">Contacto</h1>
-    """
-
-@app.route("/nosotros/")
-def about():
-    return """
-    <h1 style="font-size: 64px; color:blue;">Nosotros</h1>
-    """
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit() and request.method == "POST":
+        return redirect(url_for("signin"))
+    return render_template("signup.html", form=form)
 
 
-@app.route("/login/", methods=["GET", "POST"])
-def login():
+@app.route("/signin", methods=["POST", "GET"])
+def signin():
     form = LoginForm()
     if form.validate_on_submit() and request.method == "POST":
-        username = form.username.data
-        password = form.password.data
-        if username and password:
-            session["username"] = username
-            return redirect(url_for("dashboard", name=username))
-        flash("Por favor ingresa correctamente tus datos.", "error")
-    return render_template("login.html", form = form)
+        return render_template("signin.html")
+    return render_template("signin.html", form=form)
 
-@app.route("/profile/<name>/")    
-@app.route("/profile/")
-def dashboard(name=None):
-    if name is not None:
-        flash(f"Bienvenido(a) {name}", "success")
-        return render_template("dashboard.html", username=name)
-    return redirect(url_for("login"))
 
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(url_for("login"))
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
