74043838b6b178b2756bc20a8675db5fc7ad46d5
Miguel Bohórquez <dlsg1009@gmail.com>
2023-09-18 20:10:48 -0500

    Agregando models.py

--- models.py
+++ models.py
@@ -4,19 +4,21 @@ from werkzeug.security import check_password_hash, generate_password_hash
 db_user = []
 
 class User(UserMixin):
-
+    
     def __init__(self, id, firstname, lastname, email, password):
         self.id = id
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
         self.password = generate_password_hash(password)
-
+    
+    # para verificar la contraseña del usuario
     def verify_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.password, password) 
         
 def get_user(email):
     for user in db_user:
         if user.email == email:
             return user
-        return False
+        return None
+
