421b0318a2a9bbe66803a3ce2ea7c64697e9eae2
Miguel Bohórquez <dlsg1009@gmail.com>
2023-09-18 19:37:42 -0500

    Agregando models.py

--- models.py
+++ models.py
@@ -1,23 +1,22 @@
 from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db_user = []
 
 class User(UserMixin):
 
-    def __init__(self, firstname, lastname, email, password):
+    def __init__(self, id, firstname, lastname, email, password):
+        self.id = id
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
-        self.password = password
+        self.password = generate_password_hash(password)
 
     def verify_password(self, password):
-        if self.password == password:
-            return True
-        else:
-            return False
+        return check_password_hash(self.password, password)
         
 def get_user(email):
     for user in db_user:
         if user.email == email:
-            return True
+            return user
         return False
